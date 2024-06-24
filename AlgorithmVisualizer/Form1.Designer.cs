ccc837b01ae4ac988db061f9f1cec2aefde21258
Alessandro <alecolin96@gmail.com>
2024-06-24 19:40:27 +0200

    First test commit

--- AlgorithmVisualizer/Form1.Designer.cs
+++ AlgorithmVisualizer/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxAlgorithmSelector = new System.Windows.Forms.ComboBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.panelGraphic = new System.Windows.Forms.Panel();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Algorithms";
+            // 
+            // comboBoxAlgorithmSelector
+            // 
+            this.comboBoxAlgorithmSelector.FormattingEnabled = true;
+            this.comboBoxAlgorithmSelector.Location = new System.Drawing.Point(88, 39);
+            this.comboBoxAlgorithmSelector.Name = "comboBoxAlgorithmSelector";
+            this.comboBoxAlgorithmSelector.Size = new System.Drawing.Size(171, 24);
+            this.comboBoxAlgorithmSelector.TabIndex = 2;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(888, 37);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(85, 29);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // panelGraphic
+            // 
+            this.panelGraphic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelGraphic.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelGraphic.Location = new System.Drawing.Point(12, 93);
+            this.panelGraphic.Name = "panelGraphic";
+            this.panelGraphic.Size = new System.Drawing.Size(977, 393);
+            this.panelGraphic.TabIndex = 4;
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.AutoSize = false;
+            this.trackBarSpeed.Location = new System.Drawing.Point(562, 39);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(320, 27);
+            this.trackBarSpeed.TabIndex = 5;
+            this.trackBarSpeed.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(508, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Speed";
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(275, 37);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(101, 27);
+            this.buttonSort.TabIndex = 7;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1001, 498);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.panelGraphic);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.comboBoxAlgorithmSelector);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Algorithm Visualizer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxAlgorithmSelector;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Panel panelGraphic;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSort;
     }
 }
 
--- AlgorithmVisualizer/Form1.cs
+++ AlgorithmVisualizer/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace AlgorithmVisualizer
 {
     public partial class Form1 : Form
     {
+        #region Fields
+        int[] arrayOfNumbers;
+        int numEntries;
+        int maxValue;
+        Graphics g;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
         public Form1()
         {
             InitializeComponent();
+
+            LoadDefault();
         }
+        #endregion
+
+        #region Methods
+        private void LoadDefault()
+        {
+            // At first initialization the number of entries and their maximum number is defined by the width and height of the panel.
+            numEntries = panelGraphic.Width / 2;
+            maxValue = panelGraphic.Height;
+            trackBarSpeed.SetRange(1, panelGraphic.Width / 2);
+            trackBarSpeed.Value = numEntries / 2;
+        }
+        // Create Random Values and Bars.
+        private void CreateRandomValuesBars()
+        {
+            // If the arrayOfNumbers is empty recreate the entire array of values. 
+
+        }
+        #endregion
+
+        #region Event Handlers
+        // Cloes the application when the exit button is pressed.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // Action to be performed at the Reset button click. Re-intialization of the array of numbers.
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            // Create a graphic subject on the Form.
+            g = panelGraphic.CreateGraphics();
+            // Set constraints for the numbers of random numbers created and the maximum values that can have based on the panel properties.
+            int numEntries = this.numEntries;
+            int maxValue = this.maxValue;
+            // Initialize the background of the panel to the black color.
+            g.FillRectangle(new SolidBrush(Color.Black), 0, 0, panelGraphic.Width, panelGraphic.Height);
+            // Creating a random array of values.
+            arrayOfNumbers = new int[numEntries];
+            Random random = new Random();
+            for (int i = 0; i < numEntries; i++)
+                arrayOfNumbers[i] = random.Next(maxValue);
+
+            // Retrieve the width of each of the bar/rectangle.
+            int rectangleWidth = (int)(panelGraphic.Width / numEntries != 0 ? numEntries : 1);
+            // Drawing a bars for each of the random number in the array.
+            for (int i = 0; i < numEntries; i++)
+            {
+                // Retrieve the dimension of each of the bar.
+                //g.DrawRectangle(new SolidBrush(Color.White), new Rectangle());
+            }
+        }
+        // Action to be performed when the scroll bar changes value.
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            this.numEntries = trackBarSpeed.Value;
+            
+            CreateRandomValuesBars();
+        }
+        #endregion
     }
 }
